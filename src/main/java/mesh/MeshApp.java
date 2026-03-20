920a7893477e403ade1957da303a92d2283022e7
BogDeepSeek <kkorsakov47@gmail.com>
2026-03-20 22:47:20 +0700

    Дизайн 1.1.0

--- src/main/java/mesh/MeshApp.java
+++ src/main/java/mesh/MeshApp.java
@@ -3,22 +3,27 @@ package mesh;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import mesh.controllers.MainController;
 
 public class MeshApp extends Application {
 
-
     @Override
     public void start(Stage primaryStage) throws Exception {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/main.fxml"));
-        AnchorPane root = loader.load();
+        VBox root = loader.load();
 
-        primaryStage.setTitle("Meshenger");
-        primaryStage.setScene(new Scene(root));
-        primaryStage.setResizable(false);
+        // Убираем стандартный заголовок окна
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+
+        Scene scene = new Scene(root);
+
+        // Подключаем CSS (если еще не подключен в FXML)
+        scene.getStylesheets().add(getClass().getResource("/css/messenger.css").toExternalForm());
+
+        primaryStage.setScene(scene);
         primaryStage.setOnCloseRequest(event -> {
             MainController controller = loader.getController();
             controller.shutdown();
